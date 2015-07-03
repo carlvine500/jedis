8f6529d89a7e5f626028f841feccf3bdde734d96
unknown <carlvine500@gmail.com>
2015-07-03 17:57:51 +0800

    support read from slaves

---
+++ src/test/java/redis/clients/jedis/tests/ClusterConcurrentTest.java
@@ -0,0 +1,45 @@
+package redis.clients.jedis.tests;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import com.taobao.stresstester.StressTestUtils;
+import com.taobao.stresstester.core.StressTask;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class ClusterConcurrentTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Set<HostAndPort> jedisClusterNode = new HashSet<HostAndPort>();
+		jedisClusterNode.add(new HostAndPort("10.58.22.220", 7000));
+
+		GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+		poolConfig.setMaxIdle(200);
+		poolConfig.setMaxTotal(500);
+		final JedisCluster jc = new JedisCluster(jedisClusterNode, poolConfig);
+		final byte[][] bytes = new byte[][] { { 50 }, { 51 }, { 52 } };
+		final byte[] bytes2 = new byte[5];
+		String string = jc.get("b");
+		System.out.println(string);
+		//cluster c400 qps 6.8w
+		//s c400 qps 4.7w
+		final AtomicInteger i = new AtomicInteger();
+		StressTestUtils.testAndPrint(400, 100 * 10000, new StressTask() {
+			@Override
+			public Object doTask() throws Exception {
+				byte[] bs = jc.get(bytes[i.incrementAndGet() % 3]);
+				//byte[] bs = jc.get(bytes2);
+				return null;
+			}
+		});
+	}
+
+}
---
+++ src/test/java/redis/clients/jedis/tests/ClusterSimpleTest.java
@@ -0,0 +1,52 @@
+package redis.clients.jedis.tests;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
+import com.taobao.stresstester.StressTestUtils;
+import com.taobao.stresstester.core.StressTask;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class ClusterSimpleTest {
+
+	/**
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		Set<HostAndPort> jedisClusterNode = new HashSet<HostAndPort>();
+		jedisClusterNode.add(new HostAndPort("10.58.22.220", 7000));
+
+		GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+		poolConfig.setMinIdle(8);
+		poolConfig.setMaxIdle(8);
+		poolConfig.setMaxTotal(8);
+		poolConfig.setTestWhileIdle(true);
+		final JedisCluster jc = new JedisCluster(jedisClusterNode, poolConfig);
+		final byte[][] bytes = new byte[][] { { 50 }, { 51 }, { 52 } };
+		final byte[] bytes2 = new byte[5];
+		// Thread.sleep(1000000000000L);
+		for (int i = 0; i < 100000000; i++) {
+			String string = jc.get("b");
+			System.out.println(string);
+		}
+	}
+
+	public static void testname2() throws Exception {
+		testname1();
+	}
+
+	public static void testname1() throws Exception {
+		if ("key".equals("")) {
+			System.out.println(Arrays.toString(new Throwable().getStackTrace()));
+		}
+
+	}
+
+}
